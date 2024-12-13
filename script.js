9b96c640ed2bbf8d617d6ce556cf7ce1c002c6a8
Pankil Kansal <pankilkansal3@gmail.com>
2024-12-13 04:18:58 -0500

    Implementing Scroll-to-Top Button

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ document.querySelectorAll('.card').forEach((card) => {
         alert('Card clicked');
     });
 });
+
+const scrollToTopBtn = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTopBtn.classList.add('show');
+    } else {
+        scrollToTopBtn.classList.remove('show');
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
