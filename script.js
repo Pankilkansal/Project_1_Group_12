a5db89e5383a635f14e76cd1a04f3a25e406c296
Pankil Kansal <pankilkansal3@gmail.com>
2024-12-13 04:17:12 -0500

    Handling Click Event on Cards

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-// script.js
+
 document.querySelector('.menu').addEventListener('click', () => {
     document.querySelectorAll('.navbar-link').forEach((item) => {
         item.classList.toggle('change');
+    });
+});
+const icons = document.querySelectorAll('.section-1-icons i');
+let i = 0;
+
+setInterval(() => {
+    icons[i].classList.remove('change'); // Remove 'change' from current icon
+    i = (i + 1) % icons.length; // Loop back to first icon after last
+    icons[i].classList.add('change'); // Add 'change' to the new icon
+}, 4000);
+
+document.querySelectorAll('.card').forEach((card) => {
+    card.addEventListener('click', () => {
+        alert('Card clicked');
     });
 });
